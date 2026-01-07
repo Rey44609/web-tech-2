6f6dec2f73e71c41f3f146ffdd4b15fb8de74d4e
Rey Benedict Tresmaria <rbtresmaria44609@liceo.edu.ph>
2026-01-07 12:58:32 +0800

    Prelim Exam submission

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import {  ActivityComponent } from './activity/activity-1.component';
+import { PrelimExamComponent } from './prelim-exam/prelim-exam.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'activity-1',
+        component: ActivityComponent},
+
+        {
+          path: 'prelim-exam',
+          component: PrelimExamComponent
+        }
+
+
+ 
+];
